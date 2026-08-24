aefbbb5ec1cb894e7675aa811816af7c8a61d289
koleenciacico <koleenciacico021@gmail.com>
2026-08-24 19:16:56 +0800

    fix the etch-a-sketch

--- scripts.js
+++ scripts.js
@@ -1,7 +1,32 @@
+const parentElement = document.body;
+
 const parentContainer = document.getElementById('parent-container');
 
-for (let i = 0; i < 256; i++) {
-  const gridDiv = document.createElement('div');
-  gridDiv.className = 'gridDiv';
-  parentContainer.appendChild(gridDiv);
+function createGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const gridDiv = document.createElement('div');
+    gridDiv.className = 'gridDiv';
+    parentContainer.appendChild(gridDiv);
+
+    gridDiv.addEventListener('mouseover', (event) => {
+      event.target.classList.add('hovered');
+    });
+
+    gridDiv.style.width = `${960 / size}px`;
+    gridDiv.style.height = `${960 / size}px`;
+  }
 }
+createGrid(16);
+
+const askGrid = document.getElementById('new-grid-btn');
+askGrid.addEventListener('click', () => {
+  let numGrid = prompt('Change the number of grid you desire');
+  let resize = parseInt(numGrid, 10);
+
+  if (resize > 100 || resize <= 0 || isNaN(resize)) {
+    alert('You enter invalid input');
+  } else {
+    parentContainer.innerHTML = '';
+    createGrid(resize);
+  }
+});
